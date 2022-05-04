89adb1c011cdf0d4388e0e1f85596e34c8095b66
Ivan <bykoviv78@gmail.com>
2022-05-04 23:30:24 +0700

    1

--- movie_proj/settings.py
+++ movie_proj/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movie_app'
+    'movie_app',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
